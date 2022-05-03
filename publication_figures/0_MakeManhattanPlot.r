415c59275704d29715d23eb6a7a714c6907841f1
Jason G. Wallace <jason.wallace@uga.edu>
2022-05-03 10:46:49 -0400

    Update manhattans to only do RMIP

--- publication_figures/0_MakeManhattanPlot.r
+++ publication_figures/0_MakeManhattanPlot.r
@@ -60,11 +60,11 @@ plot_manhattan = function(trait_name, glm_file, farm_file, args){
 
     # Load in data
     cat("Plotting data for",trait_name, "\n")
-    glm = load_glm(glm_file, debug=args$debug)
+    #glm = load_glm(glm_file, debug=args$debug)
     farm = load_farm(farm_file)
     
-    # Subset out permutation significance
-    glm.sig = subset(glm, glm$perm_p <= args$perm_cutoff)
+    # # Subset out permutation significance
+    # glm.sig = subset(glm, glm$perm_p <= args$perm_cutoff)
     
     # Reformat RMIP data
     farm.all = farm # Backup of complete RMIP data
@@ -73,39 +73,58 @@ plot_manhattan = function(trait_name, glm_file, farm_file, args){
     
     # Set up plotting colors
     chrom_colors=RColorBrewer::brewer.pal(3, "Set1")[1:2]
-    my_alpha = 0.5
-    if(all(glm$Pos==1)){ # Set alpha to 0 for GLM if is a dummy dataset (all positions at 1)
-        my_alpha=0
-    }
+    # my_alpha = 0.5
+    # if(all(glm$Pos==1)){ # Set alpha to 0 for GLM if is a dummy dataset (all positions at 1)
+    #     my_alpha=0
+    # }
  
-    # Basic plotting
-    myplot = ggplot(glm, mapping=aes(x=get_offset(Chr, Pos), y=log.p, color=(Chr%%2 == 0))) + 
-        geom_point(alpha=my_alpha) +  # Make a scatterplot
-        scale_color_manual(values=chrom_colors) +
+    # # Basic plotting
+    # myplot = ggplot(glm, mapping=aes(x=get_offset(Chr, Pos), y=log.p, color=(Chr%%2 == 0))) + 
+    #     geom_point(alpha=my_alpha) +  # Make a scatterplot
+    #     scale_color_manual(values=chrom_colors) +
+    #     theme_few() + # Low-clutter theme
+    #     scale_x_continuous(breaks=offsets$midpoint, minor_breaks = offsets$offset, labels=rownames(offsets)) +  # Set labels and line positions for X axis
+    #     theme(legend.position = "none") +  # Turn off legend
+    #     #labs(x="Chromosome", y=expression(bold(paste("-", log[10], " p-value (GLM)")))) + # X and Y axis labels
+    #     labs(x=element_blank(), y=element_blank()) + # X and Y axis labels
+    #     ggtitle(trait_name) +
+    #     theme(title = element_text(size=10, face='bold')) # Set title 
+    # 
+    # # Add significant GLM hits
+    # myplot = myplot +
+    #     geom_point(data=glm.sig, size=2, color='black', fill=alpha('white', 0), shape=21, stroke=1.5)
+    # 
+    # # Add right Y axis to plot
+    # if(nrow(farm.all)>0){
+    #     farm.max = max(farm.all$rmip)
+    # }else{
+    #     farm.max=1
+    # }
+    # scale_trans = farm.max / max(glm$log.p)    
+    # myplot = myplot +
+    #     #scale_y_continuous(sec.axis = sec_axis(trans = ~. * scale_trans, name="RMIP (FARM-CPU)"))
+    #     scale_y_continuous(sec.axis = sec_axis(trans = ~. * scale_trans, name=element_blank()))
+    
+    # Significant FarmCPU hits by RMIP
+    myplot = ggplot(farm, mapping=aes(x=get_offset(Chr, Pos), y=rmip)) + 
+        #geom_point(shape=25, size=2, color="black", fill="black") +  # Make a scatterplot
+        #scale_color_manual(values=chrom_colors) +
+        #scale_fill_manual(values=chrom_colors) +
         theme_few() + # Low-clutter theme
         scale_x_continuous(breaks=offsets$midpoint, minor_breaks = offsets$offset, labels=rownames(offsets)) +  # Set labels and line positions for X axis
         theme(legend.position = "none") +  # Turn off legend
         #labs(x="Chromosome", y=expression(bold(paste("-", log[10], " p-value (GLM)")))) + # X and Y axis labels
         labs(x=element_blank(), y=element_blank()) + # X and Y axis labels
         ggtitle(trait_name) +
-        theme(title = element_text(size=10, face='bold')) # Set title 
-        
+        theme(title = element_text(size=10, face='bold')) # Set title
     
-    # Add significant GLM hits
-    myplot = myplot +
-        geom_point(data=glm.sig, size=2, color='black', fill=alpha('white', 0), shape=21, stroke=1.5)
-    
-    # Adjust Y axis - add right axis and set significant digits for everything
-    if(nrow(farm.all)>0){
-        farm.max = max(farm.all$rmip)
-    }else{
-        farm.max=1
-    }
-    scale_trans = farm.max / max(glm$log.p)    
-    myplot = myplot +
-        scale_y_continuous(labels = scales::number_format(accuracy=0.1),
-                           sec.axis = sec_axis(trans = ~. * scale_trans, name=element_blank(),
-                                               labels = scales::number_format(accuracy=0.01)))
+    # if(nrow(farm) > 0){
+    #     farm$rmip.plot = rescale(farm$rmip, from=c(0, max(farm$rmip)), to=c(0, max(glm$log.p)))
+    #     rmip_color="black"
+    #     myplot = myplot +
+    #         #geom_segment(data=farm, mapping=aes(xend=get_offset(Chr, Pos), y=0, yend=rmip.plot), color=rmip_color, size=0.5) +
+    #         geom_point(data=farm, mapping=aes(y=rmip.plot), size=2, color=rmip_color, fill=rmip_color, shape=25) 
+    # }
     
     # Cumulative Farm-CPU distribution
     if(nrow(farm.all) > 0){
@@ -122,27 +141,33 @@ plot_manhattan = function(trait_name, glm_file, farm_file, args){
             }
             
             # Get translated Y values
-            mypoints$rmip.plot = rescale(mypoints$cum_rmip, from=c(0, max(farm.all$rmip)), to=c(0, max(glm$log.p)))
+            #mypoints$rmip.plot = rescale(mypoints$cum_rmip, from=c(0, max(farm.all$rmip)), to=c(0, max(glm$log.p)))
+            #mypoints$mycolor=ifelse(as.numeric(mypoints$Chr)%%2==0, yes="red", no="blue")
             
             # Add to plot
+            mycolor = ifelse(as.numeric(mychrom) %% 2 ==0, yes=chrom_colors[1], no=chrom_colors[2])
             myplot = myplot +
-                geom_line(data=mypoints, mapping=aes(y=rmip.plot), color=cum_color, size=0.35) 
+                #geom_line(data=mypoints, mapping=aes(y=rmip.plot), color=cum_color, size=0.35)
+                geom_line(data=mypoints, mapping=aes(y=cum_rmip), color=mycolor, size=0.35) 
         }
         
-        # Add horizontal reference line - DEPRECATED b/c felt more confusing; likely to be confused with GLM
+        # Add horizontal reference line
         #ref_line_y = rescale(args$rmip_cutoff, from=c(0, max(farm.all$rmip)), to=c(0, max(glm$log.p)))
-        #myplot = myplot +
-        #    geom_hline(yintercept=ref_line_y, color=cum_color, size=0.35, linetype="dashed")
+        myplot = myplot +
+            geom_hline(yintercept=args$rmip_cutoff, color=cum_color, size=0.35, linetype="dashed")
     }
     
-    # Significant FarmCPU hits by RMIP
-    if(nrow(farm) > 0){
-        farm$rmip.plot = rescale(farm$rmip, from=c(0, max(farm$rmip)), to=c(0, max(glm$log.p)))
-        rmip_color="black"
-        myplot = myplot +
-            #geom_segment(data=farm, mapping=aes(xend=get_offset(Chr, Pos), y=0, yend=rmip.plot), color=rmip_color, size=0.5) +
-            geom_point(data=farm, mapping=aes(y=rmip.plot), size=2, color=rmip_color, fill=rmip_color, shape=25) 
-    }
+    myplot = myplot + 
+        geom_point(shape=25, size=2, color="black", fill="black") 
+    
+    # # Significant FarmCPU hits by RMIP
+    # if(nrow(farm) > 0){
+    #     farm$rmip.plot = rescale(farm$rmip, from=c(0, max(farm$rmip)), to=c(0, max(glm$log.p)))
+    #     rmip_color="black"
+    #     myplot = myplot +
+    #         #geom_segment(data=farm, mapping=aes(xend=get_offset(Chr, Pos), y=0, yend=rmip.plot), color=rmip_color, size=0.5) +
+    #         geom_point(data=farm, mapping=aes(y=rmip.plot), size=2, color=rmip_color, fill=rmip_color, shape=25) 
+    # }
     
     return(myplot)
 }
--- publication_figures/1_MakeCompositeFigures.r
+++ publication_figures/1_MakeCompositeFigures.r
@@ -3,10 +3,9 @@
 library(gridExtra)
 library(ggpubr)
 library(grid)
-library(cowplot)
 library(png)
 options(stringsAsFactors=F)
-setwd('~/Documents/Papers/PUNNURI_Sorghum Aphid Resistance/GWAS_github/publication_figures/')
+setwd('~/Documents/Papers/1_Submitted/PUNNURI_Sorghum Aphid Resistance/GWAS_github/publication_figures/')
 
 # Arguments to pass along (easier to keep in a list like this)
 args=list()
@@ -15,10 +14,11 @@ args$perm_cutoff=0.01
 args$winsize=5e6
 args$step=5e5
 args$offsets="sorghum_offsets.csv"
-args$debug=FALSE  # Set to true to speed plotting during debugging of changes
+args$debug=FALSE
 
 # Output figure size (in inches)
 out.width=8
+#out.height=3 # Depends on the specific plot
 out.dpi=600
 legend.file="wallace-legend.png"
 
@@ -29,10 +29,12 @@ offsets = load_offsets(args$offsets)
 # Load key connecting human-fiendly phenotype names to ones used in pipeline
 source("0_PhenotypeKey.r")
 
+
 # Helper function to make plots for each phenotype
-get_plots = function(traits){
+get_plots = function(traits, legend.height=0.04){
     file.info = pheno_name_key[traits]
-    glm_files = paste("Results_GLM/glm_results.",file.info,".csv.gz", sep='')
+    #glm_files = paste("Results_GLM/glm_results.",file.info,".csv.gz", sep='')
+    glm_files = paste("IGNORE",file.info) # Ignoring GLM results now (reviewer comments)
     farm_files = paste("Results_FarmCPU/1e_",file.info,".p05.rmip05.csv", sep='')
     
     # Make plots
@@ -40,21 +42,20 @@ get_plots = function(traits){
     for(i in 1:length(traits)){
         myplots[[i]] = plot_manhattan(traits[i], glm_files[i], farm_files[i], args)
     }
-    myplots = align_plots(plotlist=myplots, align="v")
     
     # Arrange into grid
     chrom_label=text_grob("Chromosome", size = 12, face = "bold") 
-    left_label =text_grob(expression(bold(paste("-", log[10], " p-value (GLM)"))), size = 12, rot=90) 
-    right_label =text_grob("RMIP (FarmCPU)", size = 12, face='bold', rot=-90) 
+    #left_label =text_grob(expression(bold(paste("-", log[10], " p-value (GLM)"))), size = 12, rot=90) 
+    left_label =text_grob("RMIP (FarmCPU)", size = 12, face='bold', rot=90) 
+    right_label =text_grob(expression(""), size = 12, rot=90) 
     legend=rasterGrob(readPNG(legend.file))
     grid.arrange(arrangeGrob(grobs=myplots, ncol=1), legend,
-                 nrow=2, heights=c(10,1), bottom=chrom_label, left=left_label, right=right_label)
+                 nrow=2, heights=c(1,legend.height), bottom=chrom_label, left=left_label, right=right_label)
 }
 
-
 # Aphid damage
-png("Figure - Aphid Damage Manhattan plots.png", width=out.width, height=8, units="in", res=out.dpi)
-    get_plots(c("D1 Aphid Count (log)", "D1 Aphid Damage (log)", "D2 Aphid Damage (log)","Greenhouse Damage"))
+png("Figure - Aphid Damage Manhattan plots.png", width=out.width, height=5.5, units="in", res=out.dpi)
+    get_plots(c("AC1 (log)", "D1 (log)", "D2 (log)")) #,"Greenhouse Damage"))
 dev.off()
 
 # Drones
@@ -65,8 +66,11 @@ png("Figure - Drone Manhattan plots (flowering covariate).png", width=out.width,
     get_plots(c("NDRE (flowering covariate)", "NDVI (flowering covariate)", "SAVI (flowering covariate)"))
 dev.off()
 
-# Flowering + Height
-png("Figure - Flowering and Height Manhattan plots.png", width=out.width, height=4, units="in", res=out.dpi)
-    get_plots(c("Flowering Time", "Plant Height (log)"))
+# Flowering
+png("Figure - Flowering plot.png", width=out.width, height=2.5, units="in", res=out.dpi)
+    get_plots(c("Flowering Time"), legend.height=0.1)
 dev.off()
+
+
+
 
